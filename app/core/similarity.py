b3cad59567200bd5de374f418cdd12132017a30c
Minh Tuyen <bachminhtuyen2003@gmail.com>
2025-06-01 01:20:06 +0700

    feat(AI model): Traing AI & evaluating model accuracy

--- app/core/similarity.py
+++ app/core/similarity.py
@@ -3,13 +3,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 from typing import Optional
 from app.models import Subject
+# import os
+# model = SentenceTransformer("distiluse-base-multilingual-cased-v2")  # Hỗ trợ tiếng Việt
+model = SentenceTransformer("app/core/output/subject-matching-model") # Mô hình đã training
 
-model = SentenceTransformer("distiluse-base-multilingual-cased-v2")  # Hỗ trợ tiếng Việt
+# print(os.path.exists("output/subject-matching-model"))
+# print(os.listdir("app/core/output/subject-matching-model"))
 
 async def compute_cosine_similarity(tutor_description, student_description):
     
     sentences = [tutor_description, student_description]
-    model = SentenceTransformer("distiluse-base-multilingual-cased-v2")
     
     embeddings = model.encode(sentences)
     
@@ -31,14 +34,15 @@ async def find_matching_subject(keyword: str, subjects: list) -> Optional[Subjec
     best_subject = None
 
     for subj in subjects:
-        subject_text = f"{subj.subjectName_vi} {subj.subjectName_en}"
-        subj_embedding = model.encode([subject_text])[0]
+        subject_text = f"{subj.subjectName_vi}"
+        subject_lower_text = subject_text.lower()
+        subj_embedding = model.encode([subject_lower_text])[0]
         score = cosine_similarity([keyword_embedding], [subj_embedding])[0][0]
         
         if score > best_score:
             best_score = score
             best_subject = subj
 
-    if best_score >= 0.55:
+    if best_score >= 0.6:
         return best_subject
     return None
